214826a505c8f7901fd8d25ca65bdeed77dff77b
Ian Neal <wopr@wopr.tech>
2016-11-01 22:24:52 -0600

    Even more node work

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,7 +1,47 @@
 #include "Node.h"
+#include <sstream>
 
+void Node::write_output(Frame to_write) {
+    _outfile << to_write.src() << ":" << to_write.data();
+}
 
-void Node::write_output(std::string to_write)
-{
-    _outfile << to_write;
+void Node::write_to_socket(Frame to_write) {
+    std::string frame_raw = to_write.raw();
+    _switch_socket->send(frame_raw.c_str(), frame_raw.length());
+}
+
+void Node::send_acknowledgement(Frame to_ack) {
+    to_ack.clear();
+    write_to_socket(to_ack);
+}
+
+Frame Node::read_from_input() {
+    std::string input_line, frame_data;
+    if(_infile.is_open() and _infile.good()) {
+        std::getline(_infile,input_line);
+    }
+
+    istringstream input_line_stream(input_line);
+    uint8_t dst;
+    char colon; // Garbage for the separator
+
+    input_line_stream >> dst >> colon >> frame_data;
+
+    return Frame(_num, dst, frame_data);
+}
+
+Frame Node::read_from_socket() throw(NodeException) {
+    char in_buf[MAX_FRAME_SZ+1] = {'\0'};
+
+    if(_switch_socket->recv(in_buf, MAX_FRAME_SZ) > 0) {
+        std::string frame_contents(in_buf);
+        Frame ret_frame(frame_contents);
+        return ret_frame;
+    } else {
+        throw NodeException("Switch disconnected!");
+    }
+}
+
+bool Node::is_mine(Frame to_check) {
+    return to_check.dst() == _num;
 }
--- src/Node.h
+++ src/Node.h
@@ -1,20 +1,36 @@
 #pragma once
 
+#include "PracticalSocket.h"
+#include "Frame.h"
 #include <cstdint>
 #include <string>
 #include <fstream>
 
+class NodeException : public exception {
+    string userMessage;
+public:
+    NodeException(const string &message) throw() : userMessage(message) {};
+    ~NodeException() throw() {};
+    const char *what() const throw() {return userMessage.c_str();};
+};
+
 class Node {
 public:
     // Methods
-    void write_output(std::string to_write);
+    void write_output(Frame to_write);
+    void write_to_socket(Frame to_write);
+    void send_acknowledgement(Frame to_ack);
+    Frame read_from_socket();
+    Frame read_from_input();
+    bool is_mine(Frame to_check);
 
     // Constructors
-    Node(uint8_t num) : _num {num}, _infile(("Node" + std::to_string(num) + "Input.txt").c_str()), _outfile(("Node" + std::to_string(num) + "Output.txt").c_str()) {};
+    Node(uint8_t num) : _num {num}, _infile(("Node" + std::to_string((unsigned int) num) + "Input.txt").c_str()), _outfile(("Node" + std::to_string((unsigned int) num) + "Output.txt").c_str()) {};
     Node(const Node&) = delete;
     Node(Node&&) = delete;
 protected:
-    int _num;
+    uint8_t _num;
     std::ifstream _infile;
     std::ofstream _outfile;
+    TCPSocket *_switch_socket;
 };
