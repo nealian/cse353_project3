d16ff64f87b6ecc8dab92a70c5b10a0d3c101894
Ian Neal <wopr@wopr.tech>
2016-11-05 18:19:02 -0600

    Frame priority and comparison based on priority

--- src/Frame.h
+++ src/Frame.h
@@ -3,28 +3,31 @@
 #include <string>
 #include <cstdint>
 
-#define MAX_FRAME_SZ 258 // 1 byte for each SRC, DST, and SIZE/ACK, and 255 bytes for DATA.
-
 class Frame {
 public:
-  // Getters
-  uint8_t src();
-  uint8_t dst();
-  uint8_t size();
-  std::string data();
-  std::string raw();
+    // Getters
+    uint8_t src();
+    uint8_t dst();
+    uint8_t size();
+    std::string data();
+    std::string raw();
 
-  // Setters
-  void clear();
+    // Setters
+    void clear();
 
-  // Constructors
-  Frame(std::string contents);
-  Frame(uint8_t src, uint8_t dst, std::string data) : _SRC{src}, _DST{dst}, _DATA{data} {};
-  Frame(const Frame &) = default; // For function calls
-  Frame(Frame &&) = default; // For return values
+    // Constructors
+    Frame(std::string contents);
+    Frame(uint8_t src, uint8_t dst, std::string data): _SRC {src}, _DST {dst}, _DATA {data}, _priority {0} {};
+    Frame(uint8_t src, uint8_t dst, std::string data, uint8_t priority) : _SRC {src}, _DST {dst}, _DATA {data}, _priority {priority} {}
+    Frame(const Frame&) = delete;
+    Frame(Frame&&) = delete;
+
+    // Operators
+    bool operator <(const Frame f2);
 
 protected:
-  uint8_t _SRC;
-  uint8_t _DST;
-  std::string _DATA;
+    uint8_t _SRC;
+    uint8_t _DST;
+    std::string _DATA;
+    uint8_t _priority;
 };
