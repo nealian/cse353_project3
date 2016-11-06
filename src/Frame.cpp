7f0e511176bf3d78336462326f1600f3fb90e35f
Ian Neal <wopr@wopr.tech>
2016-11-05 18:19:02 -0600

    Frame priority and comparison based on priority

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -27,7 +27,14 @@ std::string Frame::raw()
     std::string dst_raw (1, (char) _DST);
     std::string size_raw (1, (char) _DATA.length());
 
-    return src_raw + dst_raw + size_raw + _DATA;
+    if (_priority == 0) {
+        return src_raw + dst_raw + size_raw + _DATA;
+    } else {
+        std::string priority_raw (1, (char) _priority);
+        std::string zero_raw (1, 0);
+
+        return src_raw + zero_raw + size_raw + dst_raw + priority_raw + _DATA;
+    }
 }
 
 
@@ -39,9 +46,21 @@ void Frame::clear()
 
 
 /* Constructors */
-Frame::Frame(std::string contents)
-{
+Frame::Frame(std::string contents) {
     _SRC = (uint8_t) contents[0];
     _DST = (uint8_t) contents[1];
-    _DATA = contents.substr(3, (size_t) contents[2]);
+    if(_DST == 0) {
+        _DST = (uint8_t) contents[3];
+        _priority = (uint8_t) contents[4];
+        _DATA = contents.substr(5, (size_t) contents[2]);
+    } else {
+        _priority = 0;
+        _DATA = contents.substr(3, (size_t) contents[2]);
+    }
+}
+
+
+/* Operators */
+bool Frame::operator <(const Frame f2) {
+    return _priority < f2._priority;
 }
