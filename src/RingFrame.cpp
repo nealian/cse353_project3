26dc7a28612acd833363c17feb29f189c8a8fa87
Ian Neal <wopr@wopr.tech>
2016-11-29 22:00:06 -0700

    Create initial ring frame

---
+++ src/RingFrame.cpp
@@ -0,0 +1,59 @@
+#include "RingFrame.h"
+
+/* Getters */
+bool RingFrame::tok() {
+  return _tok;
+}
+
+uint8_t RingFrame::src() {
+  return _SRC;
+}
+
+uint8_t RingFrame::dst() {
+  return _DST;
+}
+
+uint8_t RingFrame::size() {
+  return _DATA.length();
+}
+
+std::string RingFrame::data() {
+  return _DATA;
+}
+
+std::string RingFrame::raw() {
+  std::string src_raw(1, (char) _ack ? 1 : 0);
+  std::string src_raw(1, (char) _SRC); // Using the fill(n, char) constructor
+  std::string dst_raw(1, (char) _DST);
+  std::string size_raw(1, (char) size());
+
+  if (_priority == 0) {
+    return src_raw + dst_raw + size_raw + _DATA + "\n";
+  } else {
+    std::string priority_raw (1, (char) _priority);
+    std::string zero_raw (1, 0);
+
+    return src_raw + zero_raw + size_raw + dst_raw + priority_raw + _DATA + "\n";
+  }
+}
+
+
+/* Setters */
+void RingFrame::ack() {
+  _ack = true;
+}
+
+
+/* Constructors */
+RingFrame::RingFrame(std::string contents) {
+  _SRC = (uint8_t) contents[0];
+  _DST = (uint8_t) contents[1];
+  if(_DST == 0) {
+    _DST = (uint8_t) contents[3];
+    _priority = (uint8_t) contents[4];
+    _DATA = contents.substr(5, (size_t) contents[2]);
+  } else {
+    _priority = 0;
+    _DATA = contents.substr(3, (size_t) contents[2]);
+  }
+}
---
+++ src/RingFrame.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+#define MAX_FRAME_SZ 258 // 1 byte for each SRC, DST, and SIZE/ACK, and 255 bytes for DATA.
+
+class RingFrame {
+public:
+  // Getters
+  bool tok();
+  uint8_t src();
+  uint8_t dst();
+  uint8_t size();
+  std::string data();
+  std::string raw();
+
+  // Setters
+  void ack();
+
+  // Constructors
+  RingFrame(std::string contents);
+  RingFrame(bool tok);
+  RingFrame(uint8_t src, uint8_t dst, std::string data, uint8_t priority) :
+      _SRC{src}, _DST{dst}, _DATA{data}, _priority{priority} {};
+  RingFrame(uint8_t src, uint8_t dst, std::string data) : RingFrame(src, dst, data, 0) {};
+  RingFrame(const RingFrame &) = default; // For function parameters
+  RingFrame(RingFrame &&) = default; // For function return values
+
+  // Operators
+  bool operator<(const RingFrame f2) const {return _priority < f2._priority;};
+  RingFrame& operator=(const RingFrame&) = default;
+
+protected:
+  bool _tok = false;
+  uint8_t _SRC;
+  uint8_t _DST;
+  std::string _DATA;
+  uint8_t _priority;
+  bool _ack;
+};
