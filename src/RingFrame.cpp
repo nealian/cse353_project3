e7c8914414e8150885f0032dc5829e75b9fbe881
Ian Neal <wopr@wopr.tech>
2016-12-02 22:02:46 -0700

    Finish implementing Ring Frames, and fix Star Frames along the way

--- src/RingFrame.cpp
+++ src/RingFrame.cpp
@@ -21,20 +21,39 @@ std::string RingFrame::data() {
   return _DATA;
 }
 
-std::string RingFrame::raw() { // TODO FIXME
-  std::string src_raw(1, (char) _ack ? 1 : 0);
-  std::string src_raw(1, (char) _SRC); // Using the fill(n, char) constructor
-  std::string dst_raw(1, (char) _DST);
-  std::string size_raw(1, (char) size());
+std::string RingFrame::raw_no_crc() {
+  if (_tok) {
+    std::string ac_fc_raw(1, 1);
 
-  if (_priority == 0) {
-    return src_raw + dst_raw + size_raw + _DATA + "\n";
+    return ac_fc_raw + ac_fc_raw;
   } else {
-    std::string priority_raw (1, (char) _priority);
-    std::string zero_raw (1, 0);
+    std::string zero_as_string(1, 0);
+    std::string src_raw(1, (char) _SRC); // Using the fill(n, char) constructor
+    std::string dst_raw(1, (char) _DST);
+    std::string size_raw(1, (char) size());
 
-    return src_raw + zero_raw + size_raw + dst_raw + priority_raw + _DATA + "\n";
+    return zero_as_string + zero_as_string + dst_raw + src_raw + size_raw + _DATA;
   }
+}
+
+uint8_t RingFrame::crc() {
+  auto raw_str = raw_no_crc();
+
+  std::vector<unsigned char> bytes(raw_str.begin(), raw_str.end());
+
+  uint8_t crc = 0;
+
+  for (auto b : bytes) {
+    crc += b;
+  }
+
+  return crc;
+}
+
+std::string RingFrame::raw() {
+  std::string ack_raw(1, (char) _ack ? 3 : 0);
+  std::string crc_raw(1, (char) crc());
+  return raw_no_crc() + crc_raw + ack_raw + "\n";
 }
 
 
@@ -48,12 +67,5 @@ void RingFrame::ack() {
 RingFrame::RingFrame(std::string contents) {
   _SRC = (uint8_t) contents[0];
   _DST = (uint8_t) contents[1];
-  if(_DST == 0) {
-    _DST = (uint8_t) contents[3];
-    _priority = (uint8_t) contents[4];
-    _DATA = contents.substr(5, (size_t) contents[2]);
-  } else {
-    _priority = 0;
-    _DATA = contents.substr(3, (size_t) contents[2]);
-  }
+  _DATA = contents.substr(3, (size_t) contents[2]);
 }
--- src/RingFrame.h
+++ src/RingFrame.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <cstdint>
+#include <vector>
 
 #define MAX_RINGFRAME_SZ 262 // 1 byte for each AC, FC, SRC, DST, and SIZE, 255 bytes for DATA, and 1
   // byte for each CRC and FS
@@ -15,21 +16,23 @@ public:
   uint8_t size();
   std::string data();
   std::string raw();
+  uint8_t crc();
 
   // Setters
   void ack();
 
   // Constructors
   RingFrame(std::string contents);
-  RingFrame(bool tok);
+  RingFrame(bool tok) : _tok {tok} {}; // Generates a token-only frame
   RingFrame(uint8_t src, uint8_t dst, std::string data, uint8_t priority) :
-      _SRC{src}, _DST{dst}, _DATA{data}, _priority{priority} {};
+      _SRC{src}, _DST{dst}, _DATA{data} {};
   RingFrame(uint8_t src, uint8_t dst, std::string data) : RingFrame(src, dst, data, 0) {};
+
   RingFrame(const RingFrame &) = default; // For function parameters
   RingFrame(RingFrame &&) = default; // For function return values
 
   // Operators
-  bool operator<(const RingFrame f2) const {return _priority < f2._priority;};
+  bool operator<(const RingFrame f2) const {return _DATA.length() < f2._DATA.length();};
   RingFrame& operator=(const RingFrame&) = default;
 
 protected:
@@ -37,6 +40,12 @@ protected:
   uint8_t _SRC;
   uint8_t _DST;
   std::string _DATA;
-  uint8_t _priority;
   bool _ack;
+<<<<<<< 85e11bde4510c585c22515a640f9b7a68b2aa14b
 };
+=======
+
+private:
+  std::string raw_no_crc();
+};
+>>>>>>> Finish implementing Ring Frames, and fix Star Frames along the way
