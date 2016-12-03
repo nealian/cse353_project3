d24daa360560ea1740391097265e103c0496c12b
Ian Neal <wopr@wopr.tech>
2016-12-02 22:02:46 -0700

    Finish implementing Ring Frames, and fix Star Frames along the way

--- src/RingFrame.h
+++ src/RingFrame.h
@@ -2,8 +2,10 @@
 
 #include <string>
 #include <cstdint>
+#include <vector>
 
-#define MAX_FRAME_SZ 258 // 1 byte for each SRC, DST, and SIZE/ACK, and 255 bytes for DATA.
+#define MAX_FRAME_SZ 262 // 1 byte for each AC, FC, SRC, DST, and SIZE, 255 bytes for DATA, and 1
+  // byte for each CRC and FS
 
 class RingFrame {
 public:
@@ -14,21 +16,23 @@ public:
   uint8_t size();
   std::string data();
   std::string raw();
+  uint8_t crc();
 
   // Setters
   void ack();
 
   // Constructors
   RingFrame(std::string contents);
-  RingFrame(bool tok);
+  RingFrame(bool tok) : _tok {tok} {}; // Generates a token-only frame
   RingFrame(uint8_t src, uint8_t dst, std::string data, uint8_t priority) :
-      _SRC{src}, _DST{dst}, _DATA{data}, _priority{priority} {};
+      _SRC{src}, _DST{dst}, _DATA{data} {};
   RingFrame(uint8_t src, uint8_t dst, std::string data) : RingFrame(src, dst, data, 0) {};
+
   RingFrame(const RingFrame &) = default; // For function parameters
   RingFrame(RingFrame &&) = default; // For function return values
 
   // Operators
-  bool operator<(const RingFrame f2) const {return _priority < f2._priority;};
+  bool operator<(const RingFrame f2) const {return _DATA.length() < f2._DATA.length();};
   RingFrame& operator=(const RingFrame&) = default;
 
 protected:
@@ -36,6 +40,8 @@ protected:
   uint8_t _SRC;
   uint8_t _DST;
   std::string _DATA;
-  uint8_t _priority;
   bool _ack;
+
+private:
+  std::string raw_no_crc();
 };
